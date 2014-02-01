8c43624015ce1ae73a83b7eb62f569308be961a6
Simon Weil <simon.weil@naturalint.com>
2014-02-01 22:54:10 +0200

    Added iterm config file and git-meld file

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/bashrc ~/.bashrc
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/pryrc ~/.pryrc
+mkdir ~/bin
+ln -s $PWD/bin/git-meld  ~/bin/git-meld
+
+# For updating the submodules of janus
+git submodule init
+git submodule update
